e05860e10cd251450f90cc6447335ddc8e46fbc1
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2024-07-22 16:59:28 -0300

    Private git repository

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -8,8 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
     }
+
 
 }
